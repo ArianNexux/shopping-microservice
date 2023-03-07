11b85a6e33b8993f3c01d3f0dd2e8464c124a21b
ArianNexux <ariannexux0101@gmail.com>
2023-03-07 20:00:38 +0000

    feat: modelate domain of product

---
+++ product-service/src/domain/entity/product.ts
@@ -0,0 +1,45 @@
+import InvalidDataProvided from "../errors/invalid-data";
+
+export default class Product {
+
+    constructor(
+        private id: string,
+        private name: string,
+        private price: number
+    ) {
+        this.validate()
+    }
+
+
+    validate() {
+        if (this.id == "") {
+            throw new InvalidDataProvided("id")
+        }
+
+        if (this.name == "") {
+            throw new InvalidDataProvided("name")
+        }
+
+        if (this.price <= 0) {
+            throw new InvalidDataProvided("price")
+        }
+    }
+
+    public getName(): string {
+        return this.name;
+    }
+
+    public getPrice(): number {
+        return this.price;
+    }
+
+    public changePrice(price: number): void {
+        this.validate()
+        this.price = price
+    }
+
+    public changeName(name: string): void {
+        this.validate()
+        this.name = name
+    }
+}
