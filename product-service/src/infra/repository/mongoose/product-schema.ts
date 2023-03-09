8916ed0def12631688c57caea97e1a620dd5e583
ArianNexux <ariannexux0101@gmail.com>
2023-03-09 17:32:15 +0000

    feat: add schema of products in mongoose

--- product-service/src/infra/repository/mongoose/product-schema.ts
+++ product-service/src/infra/repository/mongoose/product-schema.ts
@@ -1,12 +1,18 @@
-import { model, Schema } from "mongoose";
+import { model, Schema, ObjectId } from "mongoose";
 
+interface IProduct {
+    _id: string
+    name: string;
+    price: number;
+}
 
 const productSchema = new Schema<IProduct>({
+    _id: { type: String, required: true },
     name: { type: String, required: true },
     price: { type: Number, required: true }
 });
 
 
-const product = model<IProduct>('Products', productSchema)
+const productModel = model<IProduct>('Products', productSchema)
 
-export default product
+export default productModel
